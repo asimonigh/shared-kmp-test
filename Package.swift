14521818199cf96256af16ca3fca548238060803
anthony <anthony.simonigh@liberty-rider.com>
2025-04-25 18:05:07 +0200

    v 0.1.2

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "WeatherAlert",
-         url: "https://github.com/asimonigh/shared-kmp-test/releases/download/0.1.1-test/weatheralert.xcframework.zip",
-         checksum:"e80144d230ca7068db87152b0c45ccffeab296512ffd89b7cd328e849c2937ea")
+         url: "https://github.com/asimonigh/shared-kmp-test/releases/download/0.1.2-test/weatheralert.xcframework.zip",
+         checksum:"9b9730e1c356cdb6834d9bd4a6743b83016c40b58a2992495f2db40a12aebd87")
    ]
 )
